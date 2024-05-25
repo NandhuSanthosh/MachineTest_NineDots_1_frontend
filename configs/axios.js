53d97e9ab81e91842f470968b4c9aee149a7563a
NandhuSanthosh <nandhusanthosh87@gmail.com>
2024-05-25 20:26:10 +0530

    url updated

--- configs/axios.js
+++ configs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'http://drive.nandhu.shop',
 });
 
 export default axiosInstance
